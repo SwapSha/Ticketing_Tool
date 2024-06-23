52c8c25899249398bc092201bc684a68041ca879
SwapSha <iluesharma13@gmail.com>
2024-06-23 12:07:23 +0530

    update files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { Error404Component } from './pages/error-404/error-404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: LayoutComponent,
+        children:[
+            {path:'dashboard',component:DashboardComponent},
+            {path:'department',component:DepartmentComponent},
+            {path:'employee',component:EmployeeComponent},
+        ]
+    },
+    {path:'**',component:Error404Component}
+];
